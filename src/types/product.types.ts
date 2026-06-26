9898546b8d3597af8eb9b64ca1f282ee5947b039
Malsha Nishadini <malshanishadini1995@gmail.com>
2026-06-27 05:17:49 +0530

    create product type

---
+++ src/types/product.types.ts
@@ -0,0 +1,9 @@
+// src/types/product.types.ts
+export interface Product {
+    id: string;
+    name: string;
+    price: number;
+    category: 'backpack' | 'bike-light' | 't-shirt';
+}
+
+export type UserOverrides = Partial<Product>;
