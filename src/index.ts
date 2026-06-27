08f507849e8525824b78ba05b989c0bf487c58bb
Malsha Nishadini <malshanishadini1995@gmail.com>
2026-06-27 08:53:36 +0530

    print order summary

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 // src/index.ts
 import type { Product } from './types/product.types.js';
+import type { Order } from './types/order.types.js';
 import { lockedUser, standardUser } from './test-data/users.js';
 import { createUser } from './factories/userFactory.js';
 import { createProduct } from './factories/productFactory.js';
-import { printUserScenario, printProductSummary} from './utils/printUtils.js';
+import { createOrder } from './factories/orderFactory.js';
+import { printUserScenario, printProductSummary, printOrderSummary} from './utils/printUtils.js';
 
 
 const adminUser = createUser({
@@ -18,6 +20,7 @@ const users = [standardUser, lockedUser, adminUser, defaultUser];
 for (const user of users) {
     printUserScenario(user);
 }
+console.log("\n");
 
 
 const products: Product[] = [
@@ -29,3 +32,21 @@ const products: Product[] = [
 for (const product of products) {
     printProductSummary(product);
 }
+console.log("\n");
+
+const backpack = createProduct();
+const bikeLight = createProduct({ id: 'product-002', name: 'Bike Light', price: 9.99, category: 'bike-light' });
+const tshirt = createProduct({ id: 'product-003', name: 'T-Shirt', price: 15.99, category: 't-shirt' });
+
+const orders: Order[] = [
+    createOrder(),
+    createOrder({ id: 'order-002', user: 'Standard User2', products: [backpack], totalPrice: 9.99, status: 'Shipped' }),
+    createOrder({ id: 'order-003', user: 'Standard User3', products: [backpack, tshirt], totalPrice: 45.98, status: 'Delivered' })
+];
+
+for (const order of orders) {
+    printOrderSummary(order);
+    console.log("\n");
+}
+
+
